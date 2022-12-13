dce9b6e4c52d7339d4a8d8ace06c1ad3fae5bc95
natanni51 <natnaelendale51@gmail.com>
2022-12-13 11:10:51 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installs puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
