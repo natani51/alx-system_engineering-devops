178c5536867800fdb15ea6d9bb988c3ed21530a5
natani51 <natnaelendale51@gmail.com>
2023-02-08 07:19:55 +0000

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+# increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
