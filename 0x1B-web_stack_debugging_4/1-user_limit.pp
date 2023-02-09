3e10a4576250fea736d6545ebd03f97571f3454f
natani51 <natnaelendale51@gmail.com>
2023-02-09 13:29:56 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,4 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
-
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Remove holberton user file limits
+exec { 'Remove Limits':
+  command  => '/usr/bin/env sed -i /holberton/d /etc/security/limits.conf',
 }
